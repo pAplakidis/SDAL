980e83a0c1f58e9914cca7157b123f7fd8054363
pAplakidis <aplakidisp@gmail.com>
2023-01-06 20:10:32 +0200

    segmentation skeleton

---
+++ segmentation/train.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SegNet(nn.Module):
+  def __init__(self):
+    pass
+
+  def forward(self, x):
+    return x
+
+
+def train():
+  pass
+
+def eval():
+  pass
+
+def save_model():
+  pass
+
+def load_model():
+  pass
+
+
+if __name__ == '__main__':
+  print("Hello")
+
